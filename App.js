748d5137adc59a19c408fbbaeb57619a9c858224
psmanyam <72400185+psmanyam@users.noreply.github.com>
2020-10-31 22:18:32 +0530

    Update App.js
    state

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App(){
-  return(
-<div>
-<p>subbu</p>
+class App extends React.Component{
 
+state={
+persons:[
+        {name:'surahmanyam',age:24},
+        {name:'Gowri',age:22},
+        {name:'Revathi',age:23}]
 
-</div>);
+  }
 
+  render(){
+   return(
+  <div className='App'>
+  <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+   <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
 
+  </div>
+  );
 
+}
 }
 
 export default App;
